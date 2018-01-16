0bf914329a85d2634a40f021b0bbe14b40a7dd4a
Henri Gil <henrigil@MacBook-Pro-de-Henri.local>
2018-01-16 19:32:36 +0100

    BDD in admin

--- esgiGes/models.py
+++ esgiGes/models.py
@@ -6,10 +6,22 @@ class Professor(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        unique_together = ('first_name', 'last_name')
 
 class Student(models.Model):
     code = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.code} {self.name}"
+
+    class Meta:
+        unique_together = ('code', 'name')
+
 
 class Image(models.Model):
     image_url = models.CharField(max_length=220)
