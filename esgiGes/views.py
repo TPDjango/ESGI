719c6338241ec014050d4f5ee854e6b8a6a92a2a
Ismael Diallo <ismadia09@gmail.com>
2018-01-16 19:01:01 +0100

    suite test routes

--- esgiGes/views.py
+++ esgiGes/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Professor
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def results(request):
-    return HttpResponse("Siiiiiiiii")
+def professors(request):
+    professors_list = Professor.objects.all()
+    output = ', '.join([p.first_name for p in professors_list])
+    return HttpResponse(output)
