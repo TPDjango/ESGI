8e242c0d0034486e63033f1807387762dd18b8ca
Henri Gil <henrigil@MacBook-Pro-de-Henri.local>
2018-01-16 19:22:52 +0100

    BDD setup

--- esgiGes/models.py
+++ esgiGes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Professor(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+
+class Student(models.Model):
+    code = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+class Image(models.Model):
+    image_url = models.CharField(max_length=220)
+    student = models.OneToOneField(Student)
+
+class Cours(models.Model):
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    student = models.ManyToManyField(Student)
