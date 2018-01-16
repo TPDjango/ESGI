1dc480d3bb75d1c8db8e574ef3e5178ff9a76880
Henri Gil <henrigil@MacBook-Pro-de-Henri.local>
2018-01-16 19:32:36 +0100

    BDD in admin

--- esgiGes/admin.py
+++ esgiGes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Professor, Student, Cours, Image
+
+admin.site.register(Professor)
+admin.site.register(Student)
+admin.site.register(Cours)
+admin.site.register(Image)
