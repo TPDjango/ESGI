2074eebba9a440989811d88e0b6ef8efd0af1b89
School <fodeguipro@gmail.com>
2018-01-17 09:47:59 +0100

    fix get professor

--- esgiGes/urls.py
+++ esgiGes/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # ex: /esgiGes/
     url(r'^$', views.index, name='index'),
+    url(r'^professors/(?P<id>\d)/$', views.get_professor, name="get_professor"),
     # ex: /esgiGes/professors/
     url(r'^professors/$', views.getProfessors, name='professors'),
     # ex: /esgiGes/students/
