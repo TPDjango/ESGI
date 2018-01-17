b14996e366e663dcd90ad5a3b0c2b59c133926cc
monceeef <moncifloukili@gmail.com>
2018-01-17 19:34:29 +0100

    added tests

--- esgiGes/tests.py
+++ esgiGes/tests.py
@@ -1,3 +1,50 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
+from django.urls import get_resolver
 
-# Create your tests here.
+
+class TestUrlsAreWorking(TestCase):
+
+    def testProfessor(self):
+        url = reverse('get_professor', kwargs={'id': 2})
+        self.assertEqual(url, '/esgi/professors/2/')
+
+
+
+    def testStudent(self):
+        url = reverse('get_student', kwargs={'id': 1})
+        self.assertEqual(url, '/esgi/students/1/')
+
+
+
+    def testImage(self):
+        url = reverse('get_image', kwargs={'id': 3})
+        self.assertEqual(url, '/esgi/images/3/')
+
+
+
+    def testCourse(self):
+        url = reverse('get_cours', kwargs={'id': 3})
+        self.assertEqual(url, '/esgi/courses/3/')
+
+
+    def testStudents(self):
+            url = reverse('students')
+            resp = self.client.get(url)
+            self.assertEqual(resp.status_code, 200)
+
+    def testProfessors(self):
+            url = reverse('professors')
+            resp = self.client.get(url)
+            self.assertEqual(resp.status_code, 200)
+
+    def testImages(self):
+            url = reverse('images')
+            resp = self.client.get(url)
+            self.assertEqual(resp.status_code, 200)
+
+
+    def testCourses(self):
+            url = reverse('courses')
+            resp = self.client.get(url)
+            self.assertEqual(resp.status_code, 200)
