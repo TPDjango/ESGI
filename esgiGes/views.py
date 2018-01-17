b3808e0319c26f96fc666de6568d38504e8e0e99
School <fodeguipro@gmail.com>
2018-01-17 14:03:31 +0100

    post debug

--- esgiGes/views.py
+++ esgiGes/views.py
@@ -86,7 +86,8 @@ def getImages(request):
         except ParseError:
             return HttpResponse(status=400)
 
-    serializer = ImageSerializer(data=data)
+    serializer = ImageSerializer(data=data.get('student'))
+    return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
     if serializer.is_valid():
         serializer.save()
         return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
